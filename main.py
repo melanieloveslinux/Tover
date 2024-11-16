abfbd8846807a1771ac068704f606797369797dd
melanieloveslinux <keltikangel236@gmail.com>
2024-11-16 03:05:02 +0000

    two basic screens added

--- main.py
+++ main.py
@@ -2,5 +2,32 @@
 # Program by Melanie
 #
 
-print("Hello World.")
-print("Tover?")
+# Importing Modules
+import time
+import random
+import tkinter as tk
+from tkinter import ttk
+
+# Classes
+class tover():
+	def __init__(self,root):
+		self.root = root
+		self.root.title("Tover")
+		self.menu()
+	def menu(self):
+		for widget in self.root.winfo_children():
+			widget.destroy()
+		self.root.configure(bg="#aaaaaa")
+		nextButton = tk.Button(self.root, text="Play", command=self.inGame)
+		nextButton.pack()
+	def inGame(self):
+		for widget in self.root.winfo_children():
+			widget.destroy()
+		self.root.configure(bg="#bbbbbb")
+
+		
+
+# Main
+root = tk.Tk()
+tover(root)
+root.mainloop()
